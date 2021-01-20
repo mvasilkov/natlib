804cca75c77b2042a6ceba5391720357c18232a7
Mark Vasilkov <MarkV@nsogroup.com>
2021-01-20 16:56:31 +0200

    Mulberry32 in TypeScript

--- typescript/natlib.d.ts
+++ typescript/natlib.d.ts
@@ -1,2 +1,4 @@
+/// <reference path="prng/Mulberry32.ts" />
+
 /// <reference path="interpolation.ts" />
 /// <reference path="NVec2.ts" />
---
+++ typescript/prng/Mulberry32.ts
@@ -0,0 +1,24 @@
+/* This file is part of natlib.
+ * https://github.com/mvasilkov/natlib
+ */
+'use strict'
+/// <reference path="../natlib.d.ts" />
+
+type uint32_t = number
+
+/** Mulberry32 PRNG */
+class Mulberry32 {
+    state: uint32_t
+
+    constructor(seed: uint32_t) {
+        this.state = seed | 0
+    }
+
+    /** Return a pseudorandom uint32. */
+    randomUint32(): uint32_t {
+        let z: uint32_t = this.state = (this.state + 0x6D2B79F5) | 0
+        z = Math.imul(z ^ (z >>> 15), z | 1)
+        z ^= z + Math.imul(z ^ (z >>> 7), z | 61)
+        return (z ^ (z >>> 14)) >>> 0
+    }
+}
