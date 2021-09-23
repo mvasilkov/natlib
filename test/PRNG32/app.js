32a805b3f2a1fdbd83a00c5a274dbfedea64207f
Mark Vasilkov <mvasilkov@gmail.com>
2021-09-23 23:43:19 +0300

    Update

--- test/PRNG32/app.js
+++ test/PRNG32/app.js
@@ -1,14 +1,22 @@
+/* This file is part of natlib.
+ * https://github.com/mvasilkov/natlib
+ */
+'use strict'
+
 import { CanvasHandle } from '../natlib/canvas/CanvasHandle.js'
 import { noiseAndDist, fisherYates } from '../natlib/test/visualizePRNG32.js'
 
 const seedInput = document.getElementById('seed')
-const runButton = document.getElementById('run')
+const startButton = document.getElementById('start')
 
 const noiseCanvas = new CanvasHandle(document.getElementById('noise'), 512, 512)
 const distCanvas = new CanvasHandle(document.getElementById('dist'), 512, 512)
 const shuffleCanvas = new CanvasHandle(document.getElementById('shuffle'), 512, 512)
 
-runButton.addEventListener('click', function () {
-    noiseAndDist('Mulberry32', +seedInput.value, noiseCanvas, distCanvas)
-    fisherYates('Mulberry32', +seedInput.value, shuffleCanvas)
+startButton.addEventListener('click', function () {
+    const prng = document.querySelector('input[name="prng"]:checked').value
+    const seed = +seedInput.value
+
+    noiseAndDist(prng, seed, noiseCanvas, distCanvas)
+    fisherYates(prng, seed, shuffleCanvas)
 })
