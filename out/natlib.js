b10dcdf3054b713a876b9145c38d336ca67785f9
Mark Vasilkov <mvasilkov@gmail.com>
2021-09-28 21:10:22 +0300

    License in files. Closes #4

--- out/natlib.js
+++ out/natlib.js
@@ -1,4 +1,5 @@
 /* This file is part of natlib.
  * https://github.com/mvasilkov/natlib
+ * MIT License | Copyright (c) 2021 Mark Vasilkov
  */
 'use strict'
--- typescript/stdint.h.ts
+++ typescript/stdint.h.ts
@@ -1,5 +1,6 @@
 /* This file is part of natlib.
  * https://github.com/mvasilkov/natlib
+ * MIT License | Copyright (c) 2021 Mark Vasilkov
  */
 'use strict'
 
