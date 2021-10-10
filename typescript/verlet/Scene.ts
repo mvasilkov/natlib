7ff34df818656eab070e4af21195f94281b42e90
Mark Vasilkov <mvasilkov@gmail.com>
2021-10-10 21:41:22 +0300

    Init Scene

---
+++ typescript/verlet/Scene.ts
@@ -0,0 +1,19 @@
+/* This file is part of natlib.
+ * https://github.com/mvasilkov/natlib
+ * MIT License | Copyright (c) 2021 Mark Vasilkov
+ */
+'use strict'
+
+/** Verlet integration scene. */
+class Scene {
+    constructor() {
+    }
+
+    /** Verlet integration loop. */
+    integrate() {
+    }
+
+    /** Solve constraints and collisions. */
+    solve() {
+    }
+}
