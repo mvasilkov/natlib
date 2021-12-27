9a8077de4fd301976f4f6bdf572b6e1468c0b03a
Mark Vasilkov <mvasilkov@gmail.com>
2021-12-27 21:49:34 +0200

    Perlin noise class

--- typescript/noise/PerlinNoise.ts
+++ typescript/noise/PerlinNoise.ts
@@ -6,6 +6,7 @@
 
 import type { uint8_t } from '../prelude'
 import type { PRNG32 } from '../prng/Mulberry32'
+import { lerp, smootherstep } from '../interpolation.js'
 import { shuffle } from '../prng/functions.js'
 
 /** Perlin noise gradient function */
@@ -35,8 +36,66 @@ function grad(n: uint8_t, x: number, y: number, z: number): number {
 }
 
 /** Build a random permutation table. */
-function buildPermutationTable(r: PRNG32): uint8_t[] {
+export function buildPermutationTable(r: PRNG32): uint8_t[] {
     const a = Array.from({ length: 256 }, (_, n) => n)
 
     return shuffle(r, a).concat(a)
+}
+
+/** Perlin noise class */
+export class PerlinNoise {
+    readonly p: readonly uint8_t[]
+
+    constructor(p: readonly uint8_t[]) {
+        this.p = p
+    }
+
+    /** 3D Perlin noise */
+    noise3(x: number, y: number, z: number): number {
+        let X = Math.floor(x)
+        let Y = Math.floor(y)
+        let Z = Math.floor(z)
+
+        x -= X
+        y -= Y
+        z -= Z
+
+        X &= 255
+        Y &= 255
+        Z &= 255
+
+        const u = smootherstep(x)
+        const v = smootherstep(y)
+        const w = smootherstep(z)
+
+        const A = this.p[X]! + Y
+        const AA = this.p[A]! + Z
+        const AB = this.p[A + 1]! + Z
+        const B = this.p[X + 1]! + Y
+        const BA = this.p[B]! + Z
+        const BB = this.p[B + 1]! + Z
+
+        return lerp(
+            lerp(
+                lerp(
+                    grad(this.p[AA]!, x, y, z),
+                    grad(this.p[BA]!, x - 1, y, z),
+                    u),
+                lerp(
+                    grad(this.p[AB]!, x, y - 1, z),
+                    grad(this.p[BB]!, x - 1, y - 1, z),
+                    u),
+                v),
+            lerp(
+                lerp(
+                    grad(this.p[AA + 1]!, x, y, z - 1),
+                    grad(this.p[BA + 1]!, x - 1, y, z - 1),
+                    u),
+                lerp(
+                    grad(this.p[AB + 1]!, x, y - 1, z - 1),
+                    grad(this.p[BB + 1]!, x - 1, y - 1, z - 1),
+                    u),
+                v),
+            w)
+    }
 }
