f72870720e2b5473f9598d560429c2f84b9ffe4f
Mark Vasilkov <mvasilkov@gmail.com>
2022-01-05 18:45:28 +0200

    2022

--- typescript/prng/sampling.ts
+++ typescript/prng/sampling.ts
@@ -1,6 +1,6 @@
 /* This file is part of natlib.
  * https://github.com/mvasilkov/natlib
- * MIT License | Copyright (c) 2021 Mark Vasilkov
+ * MIT License | Copyright (c) 2022 Mark Vasilkov
  */
 'use strict'
 
