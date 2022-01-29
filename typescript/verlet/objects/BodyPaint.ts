85b085698cd7373824b01b9a63c5f2f332dda662
Mark Vasilkov <mvasilkov@gmail.com>
2022-01-29 21:46:04 +0200

    BodyPaint

--- typescript/verlet/objects/BodyPaint.ts
+++ typescript/verlet/objects/BodyPaint.ts
@@ -10,10 +10,34 @@ import { Body } from '../Body'
 type BodyCons = new (...args: any[]) => Body
 
 /** Enable a body to be painted on canvas. */
-export function BodyPaintMixin<T extends BodyCons>(Base: T) {
+export function BodyPaintMixin<T extends BodyCons>(Base: T, color = '#29adff', colorEdges = '#ff004d') {
     return class BodyPaint extends Base implements IPaint {
         /** Paint the body. */
         paint(con: CanvasRenderingContext2D, t: number) {
+            for (const v of this.vertices) v.interpolate(t)
+
+            // Paint constraints that aren't edges
+            con.beginPath()
+
+            for (const c of this.constraints) {
+                if (c.edge) continue
+                con.moveTo(c.v0.interpolated.x, c.v0.interpolated.y)
+                con.lineTo(c.v1.interpolated.x, c.v1.interpolated.y)
+            }
+
+            con.strokeStyle = color
+            con.stroke()
+
+            // Paint edges
+            con.beginPath()
+
+            for (const c of this.edges) {
+                con.moveTo(c.v0.interpolated.x, c.v0.interpolated.y)
+                con.lineTo(c.v1.interpolated.x, c.v1.interpolated.y)
+            }
+
+            con.strokeStyle = colorEdges
+            con.stroke()
         }
     }
 }
