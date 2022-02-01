e74c7e434c498bddb5ef3a06501f665c964f6c04
Mark Vasilkov <mvasilkov@gmail.com>
2022-02-01 15:59:29 +0200

    Pointer controls class

--- typescript/controls/Pointer.ts
+++ typescript/controls/Pointer.ts
@@ -7,27 +7,81 @@
 import type { CanvasHandle } from '../canvas/CanvasHandle'
 import type { IVec2 } from '../Vec2'
 
-/** Pointer controls data structure */
-export interface IPointer<T> extends IVec2 {
-    x: number
-    y: number
-    held: boolean
-    state?: T
-}
-
 /** Pointer controls class */
-export class Pointer<T> implements IPointer<T> {
+export class Pointer implements IVec2 {
     x: number
     y: number
     held: boolean
-    state?: T
 
-    readonly canvas: CanvasHandle
+    readonly ch: CanvasHandle
 
-    constructor(canvas: CanvasHandle) {
+    constructor(ch: CanvasHandle) {
         this.x = this.y = 0
         this.held = false
 
-        this.canvas = canvas
+        this.ch = ch
+    }
+
+    /** Set the pointer position relative to the canvas. */
+    setPosition(event: MouseEvent | Touch) {
+        const r = this.ch.canvas.getBoundingClientRect()
+
+        this.x = event.clientX - r.left
+        this.y = event.clientY - r.top
+    }
+
+    /** Initialize the event handlers. */
+    addEventListeners(target: HTMLElement) {
+        // Mouse events
+        target.addEventListener('mousedown', event => {
+            event.preventDefault()
+
+            this.held = true
+            this.setPosition(event)
+        })
+
+        target.addEventListener('mousemove', event => {
+            event.preventDefault()
+
+            this.setPosition(event)
+        })
+
+        target.addEventListener('mouseup', event => {
+            event.preventDefault()
+
+            this.held = false
+        })
+
+        target.addEventListener('mouseleave', event => {
+            event.preventDefault()
+
+            this.held = false
+        })
+
+        // Touch events
+        target.addEventListener('touchstart', event => {
+            event.preventDefault()
+
+            this.held = true
+            this.setPosition(event.targetTouches[0]!)
+        })
+
+        target.addEventListener('touchmove', event => {
+            event.preventDefault()
+
+            this.setPosition(event.targetTouches[0]!)
+        })
+
+        target.addEventListener('touchend', event => {
+            event.preventDefault()
+
+            this.held = false
+        })
+
+        target.addEventListener('touchcancel', event => {
+            event.preventDefault()
+
+            this.held = false
+        })
     }
 }
