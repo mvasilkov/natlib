b09296213520c2998acc05e81db755aa139dbc54
Mark Vasilkov <mvasilkov@gmail.com>
2022-02-13 22:00:30 +0200

    Move PointerBody to a file

---
+++ typescript/verlet/objects/PointerBody.ts
@@ -0,0 +1,42 @@
+/* This file is part of natlib.
+ * https://github.com/mvasilkov/natlib
+ * MIT License | Copyright (c) 2022 Mark Vasilkov
+ */
+'use strict'
+
+import type { IVec2 } from '../../Vec2'
+import { Body } from '../Body.js'
+import { Constraint } from '../Constraint.js'
+import { Scene } from '../Scene.js'
+import { Vertex } from '../Vertex.js'
+
+export class PointerBody extends Body {
+    readonly r: number
+
+    constructor(r: number) {
+        const detached = new Scene(0, 0)
+        super(detached)
+
+        this.r = r
+
+        const a = new Vertex(this, 0, 0)
+        const b = new Vertex(this, r, 0)
+        new Constraint(this, a, b, true)
+
+        this.center = this.positions[0]!
+        this.halfExtents.set(r, r)
+    }
+
+    /** Project the body onto a unit vector. */
+    projectInterval(direction: Readonly<IVec2>) {
+        const distanceInDirection = this.center.dot(direction)
+
+        this.intervalLeft = distanceInDirection - this.r
+        this.intervalRight = distanceInDirection + this.r
+    }
+
+    setPosition(position: Readonly<IVec2>) {
+        this.positions[0]!.copy(position)
+        this.positions[1]!.set(position.x + this.r, position.y)
+    }
+}
--- typescript/verlet/objects/ScenePointer.ts
+++ typescript/verlet/objects/ScenePointer.ts
@@ -4,42 +4,7 @@
  */
 'use strict'
 
-import type { IVec2 } from '../../Vec2'
-import { Body } from '../Body.js'
-import { Constraint } from '../Constraint.js'
-import { Scene } from '../Scene.js'
-import { Vertex } from '../Vertex.js'
-
-export class PointerBody extends Body {
-    readonly r: number
-
-    constructor(r: number) {
-        const detached = new Scene(0, 0)
-        super(detached)
-
-        this.r = r
-
-        const a = new Vertex(this, 0, 0)
-        const b = new Vertex(this, r, 0)
-        new Constraint(this, a, b, true)
-
-        this.center = this.positions[0]!
-        this.halfExtents.set(r, r)
-    }
-
-    /** Project the body onto a unit vector. */
-    projectInterval(direction: Readonly<IVec2>) {
-        const distanceInDirection = this.center.dot(direction)
-
-        this.intervalLeft = distanceInDirection - this.r
-        this.intervalRight = distanceInDirection + this.r
-    }
-
-    setPosition(position: Readonly<IVec2>) {
-        this.positions[0]!.copy(position)
-        this.positions[1]!.set(position.x + this.r, position.y)
-    }
-}
+import type { Scene } from '../Scene'
 
 type SceneCons = new (...args: any[]) => Scene
 
