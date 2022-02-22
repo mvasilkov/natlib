a8a0eb44fe56df83bc84a2baff1f6fa7c0b968f1
Mark Vasilkov <mvasilkov@gmail.com>
2022-02-22 22:35:57 +0200

    Update

--- typescript/mainloop.ts
+++ typescript/mainloop.ts
@@ -8,4 +8,30 @@
 type LoopCallback = (t: number) => void
 
 export function startMainloop(update: LoopCallback, render: LoopCallback, T = 0.02) {
+    let before: number
+    let t = 0
+
+    requestAnimationFrame(init)
+
+    function init(now: number) {
+        requestAnimationFrame(loop)
+
+        before = now
+    }
+
+    function loop(now: number) {
+        requestAnimationFrame(loop)
+
+        t += (now - before) * 0.001
+        before = now
+
+        let n = 2
+
+        while (t > 0) {
+            t -= T
+            if (--n >= 0) update(T)
+        }
+
+        render(t / T + 1)
+    }
 }
