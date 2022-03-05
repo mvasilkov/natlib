a46311fec4727180ebcd5a1cc7977bcc0b9c23ab
Mark Vasilkov <mvasilkov@gmail.com>
2022-03-05 22:59:54 +0200

    Couch 2048

---
+++ examples/couch2048/functions.ts
@@ -0,0 +1,6 @@
+/* This file is part of natlib.
+ * https://github.com/mvasilkov/natlib
+ * MIT+Ethics License | Copyright (c) 2022 Mark Vasilkov
+ * See https://github.com/mvasilkov/natlib/blob/master/examples/LICENSE
+ */
+'use strict'
