44404488632daefddb07ca7a49be7b3e6dc92e63
Mark Vasilkov <mvasilkov@gmail.com>
2022-09-23 23:11:26 +0300

    textCapsule()

--- typescript/canvas/text.ts
+++ typescript/canvas/text.ts
@@ -7,3 +7,29 @@
 export function setTextStyle(con: CanvasRenderingContext2D, style: string) {
     con.font = style + `px -apple-system, 'Segoe UI', 'DejaVu Sans', system-ui, sans-serif`
 }
+
+export function textCapsule(con: CanvasRenderingContext2D, x: number, y: number, style: string, text: string, padding: number, borderRadius: number) {
+    setTextStyle(con, style)
+
+    const textMetrics = con.measureText(text)
+
+    const left = textMetrics.actualBoundingBoxLeft
+    const up = textMetrics.actualBoundingBoxAscent
+    const right = textMetrics.actualBoundingBoxRight
+    const down = textMetrics.actualBoundingBoxDescent
+
+    const paddingExt = padding - borderRadius
+    const horizontalPartLength = left + right + paddingExt + paddingExt
+    const verticalPartLength = up + down + paddingExt + paddingExt
+
+    return new Path2D(
+        `M${x - left - padding},${y - up - paddingExt}` +
+        `q0,${-borderRadius},${borderRadius},${-borderRadius}` +
+        `h${horizontalPartLength}` +
+        `q${borderRadius},0,${borderRadius},${borderRadius}` +
+        `v${verticalPartLength}` +
+        `q0,${borderRadius},${-borderRadius},${borderRadius}` +
+        `h${-horizontalPartLength}` +
+        `q${-borderRadius},0,${-borderRadius},${-borderRadius}` +
+        `z`)
+}
