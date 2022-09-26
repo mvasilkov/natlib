46e3ceeef8b318f02da15e269132ce81b8410866
Mark Vasilkov <mvasilkov@gmail.com>
2022-09-26 15:17:13 +0300

    Digital differential analyzer (DDA)

--- typescript/dda.ts
+++ typescript/dda.ts
@@ -4,7 +4,7 @@
  */
 'use strict'
 
-import { Vec2 } from './Vec2.js'
+import { IVec2, Vec2 } from './Vec2.js'
 
 /** DDA tile intersection type */
 export type DdaIntersection = Vec2 & {
@@ -14,5 +14,60 @@ export type DdaIntersection = Vec2 & {
     hitVertical?: boolean,
 }
 
+const result: DdaIntersection = new Vec2
+
 /** Return true to end the DDA loop. */
 export type DdaCallback = (x: number, y: number, hitDistance: number) => boolean | void
+
+/** Digital differential analyzer (DDA) */
+export function dda(start: Readonly<IVec2>, direction: Readonly<IVec2>, done: DdaCallback): DdaIntersection {
+    // Scaling factor
+    const Δx = Math.abs(1 / direction.x)
+    const Δy = Math.abs(1 / direction.y)
+
+    // Current tile
+    let xtile = Math.floor(start.x)
+    let ytile = Math.floor(start.y)
+
+    // Ray length
+    let xlength: number, ylength: number
+
+    // Ray direction (coarse)
+    let xdir: 1 | -1, ydir: 1 | -1
+
+    if (direction.x < 0) {
+        xlength = (start.x - xtile) * Δx
+        xdir = -1
+    }
+    else {
+        xlength = (xtile + 1 - start.x) * Δx
+        xdir = 1
+    }
+
+    if (direction.y < 0) {
+        ylength = (start.y - ytile) * Δy
+        ydir = -1
+    }
+    else {
+        ylength = (ytile + 1 - start.y) * Δy
+        ydir = 1
+    }
+
+    while (true) {
+        // Extend the shorter ray
+        if (result.hitVertical = xlength < ylength) {
+            result.hitDistance = xlength
+            xlength += Δx
+            xtile += xdir
+        }
+        else {
+            result.hitDistance = ylength
+            ylength += Δy
+            ytile += ydir
+        }
+
+        if (done(xtile, ytile, result.hitDistance)) {
+            return result.setMultiplyScalar(direction, result.hitDistance).add(start)
+        }
+    }
+}
