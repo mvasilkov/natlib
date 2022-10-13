fee48e78aef4cc5c3a022d08718a016eb86786d5
Mark Vasilkov <mvasilkov@gmail.com>
2022-10-13 19:10:06 +0300

    Update

--- typescript/interpolation.ts
+++ typescript/interpolation.ts
@@ -36,41 +36,47 @@ export function easeInOut(fn: EasingFunction): EasingFunction {
     return t => t < 0.5 ? fn(2 * t) / 2 : 1 - fn(2 - 2 * t) / 2
 }
 
-/** Position equals the square of `t`. */
+/** Quadratic ease-in function, position equals the square of `t`. */
 export function easeInQuad(t: number): number {
     return t * t
 }
 
+/** Quadratic ease-out function */
 export function easeOutQuad(t: number): number {
     return t * (2 - t)
 }
 
+/** Quadratic ease-in-out function */
 export function easeInOutQuad(t: number): number {
     return t < 0.5 ? 2 * t * t : 2 * t * (2 - t) - 1
 }
 
-/** Position equals the cube of `t`. */
+/** Cubic ease-in function, position equals the cube of `t`. */
 export function easeInCubic(t: number): number {
     return t * t * t
 }
 
+/** Cubic ease-out function */
 export function easeOutCubic(t: number): number {
     return (t - 1) ** 3 + 1
 }
 
+/** Cubic ease-in-out function */
 export function easeInOutCubic(t: number): number {
     return t < 0.5 ? 4 * t * t * t : 4 * (t - 1) ** 3 + 1
 }
 
-/** Sinusoidal easing function */
+/** Sinusoidal ease-in function */
 export function easeInSine(t: number): number {
     return 1 - Math.cos(0.5 * Math.PI * t)
 }
 
+/** Sinusoidal ease-out function */
 export function easeOutSine(t: number): number {
     return Math.sin(0.5 * Math.PI * t)
 }
 
+/** Sinusoidal ease-in-out function */
 export function easeInOutSine(t: number): number {
     return 0.5 * (1 - Math.cos(Math.PI * t))
 }
