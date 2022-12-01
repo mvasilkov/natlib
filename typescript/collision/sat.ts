89a652ae3725d339db067a416ae2e7e1f8a53be9
Mark Vasilkov <mvasilkov@gmail.com>
2022-12-01 17:45:04 +0200

    Collision detection (SAT)

--- typescript/collision/sat.ts
+++ typescript/collision/sat.ts
@@ -5,7 +5,7 @@
 'use strict'
 
 import { ShortBool, TRUE } from '../prelude.js'
-import { register0 } from '../runtime.js'
+import { register0, register1 } from '../runtime.js'
 import type { Body } from '../verlet/Body'
 import type { Constraint } from '../verlet/Constraint'
 
@@ -26,6 +26,13 @@ function projectedDistance(b0: Body, b1: Body, edge: Constraint): number {
         b0.intervalLeft - b1.intervalRight
 }
 
+// Contact properties
+const contactLine = register1
+let contactEdge: Constraint
+let contactLeftIndex: number
+let contactRightIndex: number
+let contactDistance: number
+
 /** Collision detection function using the Separating Axis Theorem (SAT) */
 export function findCollision(b0: Body, b1: Body): ShortBool {
     const length0 = b0.edges.length
@@ -37,6 +44,35 @@ export function findCollision(b0: Body, b1: Body): ShortBool {
         Math.abs(b1.center.y - b0.center.y) >= b0.halfExtents.y + b1.halfExtents.y)
         // AABBs don't overlap
         return
+
+    contactDistance = projectedDistance(b0, b1, contactEdge = b0.edges[0]!)
+    // If the intervals don't overlap, there is no collision.
+    if (contactDistance >= 0) return
+    contactLine.copy(projectionLine)
+    contactLeftIndex = b1.leftIndex
+    contactRightIndex = b1.rightIndex
+
+    // Loop over `b1.edges` then `b0.edges`, excluding `b0.edges[0]`.
+    for (let n = length0 + length1; --n > 0;) {
+        const edge = n < length0 ? b0.edges[n]! : b1.edges[n - length0]!
+        const distance = projectedDistance(b0, b1, edge)
+
+        if (distance >= 0) return
+        if (distance > contactDistance) {
+            contactDistance = distance
+            contactLine.copy(projectionLine)
+            contactEdge = edge
+            // If contact edge is in `b0`, save indices from `b1`, and vice versa.
+            if (n < length0) {
+                contactLeftIndex = b1.leftIndex
+                contactRightIndex = b1.rightIndex
+            }
+            else {
+                contactLeftIndex = b0.leftIndex
+                contactRightIndex = b0.rightIndex
+            }
+        }
+    }
 
     // If there is no separating axis, then the bodies are colliding.
     return TRUE
