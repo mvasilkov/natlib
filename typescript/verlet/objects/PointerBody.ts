0cec0e3535cd66758aa4f5e234d8a0302438360d
Mark Vasilkov <mvasilkov@gmail.com>
2022-12-03 20:41:48 +0200

    Pointer body class

---
+++ typescript/verlet/objects/PointerBody.ts
@@ -0,0 +1,34 @@
+/* This file is part of natlib.
+ * https://github.com/mvasilkov/natlib
+ * MIT License | Copyright (c) 2022 Mark Vasilkov
+ */
+'use strict'
+
+import type { IVec2 } from '../../Vec2'
+import { Body } from '../Body.js'
+import { Scene } from '../Scene.js'
+
+/** Pointer body class for collision detection */
+export class PointerBody extends Body {
+    readonly r: number
+
+    constructor(r: number) {
+        const detached = new Scene(r, r, 0)
+        super(detached)
+
+        this.halfExtents.set(this.r = r, r)
+    }
+
+    /** Project the body onto a unit vector. */
+    override projectInterval(direction: Readonly<IVec2>) {
+        const distanceInDirection = this.center.dot(direction)
+
+        this.intervalLeft = distanceInDirection - this.r
+        this.intervalRight = distanceInDirection + this.r
+    }
+
+    /** Set the pointer position. */
+    setPosition(position: Readonly<IVec2>) {
+        this.center.copy(position)
+    }
+}
