249e61eade21f4dd5d73fda51ec6e12f5cfc3e46
Mark Vasilkov <mvasilkov@gmail.com>
2022-12-22 23:23:33 +0200

    Set `new Vec2` to `@__PURE__`

--- typescript/runtime.ts
+++ typescript/runtime.ts
@@ -8,7 +8,7 @@ import { Vec2 } from './Vec2.js'
 
 // These variables are used to avoid garbage collection of
 // short-lived objects at runtime.
-export const register0 = new Vec2
-export const register1 = new Vec2
-export const register2 = new Vec2
-export const register3 = new Vec2
+export const register0 = /*@__PURE__*/ new Vec2
+export const register1 = /*@__PURE__*/ new Vec2
+export const register2 = /*@__PURE__*/ new Vec2
+export const register3 = /*@__PURE__*/ new Vec2
