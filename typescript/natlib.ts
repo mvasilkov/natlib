771e3e542a2bfaf2beaee576492c6ab56c946fc4
Mark Vasilkov <mvasilkov@gmail.com>
2022-12-31 01:38:18 +0200

    2023

--- typescript/natlib.ts
+++ typescript/natlib.ts
@@ -1,5 +1,5 @@
-/* This file is part of natlib.
+/** This file is part of natlib.
  * https://github.com/mvasilkov/natlib
- * MIT License | Copyright (c) 2022 Mark Vasilkov
+ * @license MIT | Copyright (c) 2022, 2023 Mark Vasilkov
  */
 'use strict'
