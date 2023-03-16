8b8bd6177fc59280ee6c7a5844b135ff20641928
Mark Vasilkov <mvasilkov@gmail.com>
2023-03-16 22:39:25 +0200

    Get a pseudorandom point on the unit sphere

--- typescript/prng/sampling.ts
+++ typescript/prng/sampling.ts
@@ -5,6 +5,8 @@
 'use strict'
 
 import { UINT32_MAX } from '../prelude.js'
+import { register4 } from '../runtime.js'
+import type { Vec3 } from '../Vec3'
 import type { IPrng32 } from './prng'
 
 /** Return a pseudorandom number in the range [-1, 1]. */
@@ -17,4 +19,29 @@ export function randomClosedUnit1Ball(prng: IPrng32): number {
 export function randomOpenUnit1Ball(prng: IPrng32): number {
     const n = prng.randomUint32() // Inline
     return (2 * n - UINT32_MAX) / (UINT32_MAX + 1)
+}
+
+/** Get a pseudorandom point on the unit sphere. */
+export function uniformSampleSphere(prng: IPrng32): Vec3 {
+    // Algorithm by George Marsaglia (1972)
+    let u: number, v: number, n: number
+
+    do {
+        u = randomOpenUnit1Ball(prng)
+        v = randomOpenUnit1Ball(prng)
+        n = u ** 2 + v ** 2
+    }
+    while (n >= 1)
+
+    const a = 2 * (1 - n) ** 0.5
+
+    return register4.set(a * u, a * v, 1 - 2 * n)
+}
+
+/** Get a pseudorandom point on the unit hemisphere. */
+export function uniformSampleHemisphere(prng: IPrng32, normal: Readonly<Vec3>): Vec3 {
+    if (uniformSampleSphere(prng).dot(normal) < 0) {
+        return register4.scale(-1)
+    }
+    return register4
 }
--- typescript/runtime.ts
+++ typescript/runtime.ts
@@ -5,6 +5,7 @@
 'use strict'
 
 import { Vec2 } from './Vec2.js'
+import { Vec3 } from './Vec3.js'
 
 // These variables are used to avoid garbage collection of
 // short-lived objects at runtime.
@@ -12,3 +13,8 @@ export const register0 = /*@__PURE__*/ new Vec2
 export const register1 = /*@__PURE__*/ new Vec2
 export const register2 = /*@__PURE__*/ new Vec2
 export const register3 = /*@__PURE__*/ new Vec2
+
+export const register4 = /*@__PURE__*/ new Vec3
+export const register5 = /*@__PURE__*/ new Vec3
+export const register6 = /*@__PURE__*/ new Vec3
+export const register7 = /*@__PURE__*/ new Vec3
