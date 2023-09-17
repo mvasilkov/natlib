79f06656203f92288826a838c27715b523e23ec4
Mark Vasilkov <mvasilkov@gmail.com>
2023-09-18 00:20:28 +0300

    Better PositiveInteger and NonnegativeInteger conditional types

--- typescript/prelude.ts
+++ typescript/prelude.ts
@@ -4,26 +4,40 @@
  */
 'use strict'
 
-// Types that are used for documentation.
+// Types used for documentation
 export type uint8_t = number
 export type uint16_t = number
 export type uint32_t = number
+export type uint64_t = bigint
 
 export const UINT8_MAX = 255
 export const UINT16_MAX = 65535
 export const UINT32_MAX = 4294967295
+export const UINT64_MAX = 18446744073709551615n
 
-// Type assertions
-export type PositiveInteger<T extends number> = `${T}` extends '0' | `-${any}` | `${any}.${any}` ? never : T
-export type NonnegativeInteger<T extends number> = `${T}` extends `-${any}` | `${any}.${any}` ? never : T
-
-// Extended Boolean type
+/** Extended Boolean type */
 export type ExtendedBool = boolean | 0 | 1 | undefined
 
-// Compact Boolean values
+/** Compact Boolean values */
 export const enum ShortBool { FALSE, TRUE }
 
-// Utility types
-export type MethodsReturningType<T, R> = {
-    [K in keyof T]: T[K] extends (...a: any) => R ? K : never
+/** Check if the types are equal. */
+export type Equal<Left, Right> =
+    (<T>() => T extends Left ? true : false) extends
+    (<T>() => T extends Right ? true : false) ? true : false
+
+export type PositiveInteger<Value extends number = number> =
+    // Check if Value is a variable.
+    Equal<Value, number> extends true ? Value :
+    // Value is a literal, ensure it parses as a bigint and is not negative or zero.
+    `${Value}` extends `${bigint}` ? `${Value}` extends '0' | `-${string}` ? never : Value : never
+
+export type NonnegativeInteger<Value extends number = number> =
+    // Check if Value is a variable.
+    Equal<Value, number> extends true ? Value :
+    // Value is a literal, ensure it parses as a bigint and is not negative.
+    `${Value}` extends `${bigint}` ? `${Value}` extends `-${string}` ? never : Value : never
+
+export type MethodsReturningType<T, Return> = {
+    [K in keyof T]: T[K] extends (...a: any) => Return ? K : never
 }[keyof T]
